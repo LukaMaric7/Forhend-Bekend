b9ea8f4050dbf4df9af27a7c7cfdf3f998837390
Gogic <dadogogic@gmail.com>
2017-06-06 09:58:25 +0200

    AddRegionController

---
+++ BookingAppInitial-master/BookingApp/BookingApp/Controllers/RegionController.cs
@@ -0,0 +1,124 @@
+﻿using BookingApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace BookingApp.Controllers
+{
+    [RoutePrefix("api")]
+    public class RegionController : ApiController
+    {
+        BAContext db = new BAContext();
+
+        [HttpGet]
+        [Route("region")]
+        public IQueryable<Region> GetRegions()
+        {
+            return db.Regions;
+        }
+
+        [HttpGet]
+        [Route("region/{id}")]
+        [ResponseType(typeof(Region))]
+        public IHttpActionResult GetRegion(int id)
+        {
+            Region region = db.Regions.Find(id);
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(region);
+        }
+
+        [HttpPut]
+        [Route("region/{id}")]
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutRegion(int id, Region region)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != region.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(region).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RegionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        [HttpPost]
+        [Route("region")]
+        [ResponseType(typeof(Region))]
+        public IHttpActionResult PostRegion(Region region)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Regions.Add(region);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = region.Id }, region);
+        }
+
+        [HttpDelete]
+        [Route("region/{id}")]
+        [ResponseType(typeof(Region))]
+        public IHttpActionResult DeleteRegion(int id)
+        {
+            Region region = db.Regions.Find(id);
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            db.Regions.Remove(region);
+            db.SaveChanges();
+
+            return Ok(region);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool RegionExists(int id)
+        {
+            return db.Regions.Count(e => e.Id == id) > 0;
+        }
+
+    }
+}
