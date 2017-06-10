ebd46821ead1c1a05bad74488ed38264ada4e6fe
Maric <Maric@DESKTOP-QSPK9RA>
2017-06-10 11:38:38 +0200

    Region,RegionList,AddRegion and Home component started

---
+++ Angular/Project/src/app/region-list/region-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'region-list',
+  templateUrl: './region-list.component.html',
+  styleUrls: ['./region-list.component.css']
+})
+export class RegionListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
