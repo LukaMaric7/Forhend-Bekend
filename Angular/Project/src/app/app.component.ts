0fb9807fd7cac778df27ad2b502112a433341e42
LukaMaric7 <luka94ns@gmail.com>
2017-06-12 16:10:53 +0200

    Repo restarted

--- Angular/Project/src/app/app.component.ts
+++ Angular/Project/src/app/app.component.ts
@@ -1,22 +1,17 @@
 import { Component } from '@angular/core';
 import { LocalStorageService } from './localStorage.service';
-import { AccommodationService } from 'app/accommodation/accommodation.service';
-import { Accommodation } from 'app/accommodation/accommodation.model';
-import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [LocalStorageService, AccommodationService]
+  providers: [LocalStorageService]
 })
 export class AppComponent {
   title = 'app';
-  accommodations : Accommodation[];
 
-  constructor(private localStorageService : LocalStorageService, private accommodationService : AccommodationService
-  , private route : Router) { }
+  constructor(private localStorageService : LocalStorageService) { }
 
   IsLoggedIn() : boolean {
     return this.localStorageService.IsLoggedIn();
@@ -30,10 +25,4 @@ export class AppComponent {
   isManager() : Boolean {
     return this.localStorageService.isManager();
   }
-
-  initialHomeComponent() : void {
-    this.accommodationService.getAllOData().subscribe(o => {this.accommodations = o.json(); this.route.navigate(['/home',JSON.stringify(this.accommodations)])});
-  }
 }
-
-
--- Angular/Project/src/app/home/home.component.ts
+++ Angular/Project/src/app/home/home.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LocalStorageService } from 'app/localStorage.service';
 import { Accommodation } from "app/accommodation/accommodation.model";
 import { AccommodationService } from "app/accommodation/accommodation.service";
-import { Router, ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-home',
@@ -11,16 +10,14 @@ import { Router, ActivatedRoute} from "@angular/router";
   providers: [LocalStorageService, AccommodationService]
 })
 export class HomeComponent implements OnInit {
-  jsonString : string;
   accommodations : Accommodation[];
 
-  constructor(private localStorageService : LocalStorageService, private accommodationService : AccommodationService,
-  private activatedRoute : ActivatedRoute) { 
+  constructor(private localStorageService : LocalStorageService, private accommodationService : AccommodationService) { 
     this.accommodations = [];
   }
 
   ngOnInit() {
-    this.activatedRoute.params.subscribe(params => {this.jsonString = params["acc"] ; this.accommodations = JSON.parse(this.jsonString); console.log(this.accommodations[0].AccommodationType = this.accommodations[0].AccommodationType)});
+    this.accommodationService.getAllOData().subscribe(o => {this.accommodations = o.json();console.log(this.accommodations);});
     
   }
 }
