96805d54f6af62a7780055dd82467d62df7f3a26
LukaMaric7 <luka94ns@gmail.com>
2017-06-14 10:54:09 +0200

    Database modified

--- BookingAppInitial-master/BookingApp/BookingApp/Models/Comment.cs
+++ BookingAppInitial-master/BookingApp/BookingApp/Models/Comment.cs
@@ -9,22 +9,20 @@ namespace BookingApp.Models
 {
     public class Comment
     {
+        public int Id { get; set; }
+
         [Range(0,5)]
         public int? Grade { get; set; }
 
         public string Text { get; set; }
 
         public Accommodation Accommodation { get; set; }
-
-        [Key]
-        [Column(Order = 1)]
+        
         [ForeignKey("Accommodation")]
         public int AccommodationId { get; set; }
 
         public AppUser User { get; set; }
-
-        [Key]
-        [Column(Order = 2)]
+        
         [ForeignKey("User")]
         public int UserId { get; set; }
     }
